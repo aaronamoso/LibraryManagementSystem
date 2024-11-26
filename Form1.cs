12e6e4d2bdd659b20060b71916a10bc9408741ad
JNevile <j.neville657@mybvc.ca>
2024-11-25 21:31:14 -0700

    updates to login form code

--- Form1.cs
+++ Form1.cs
@@ -9,8 +9,6 @@ namespace LibraryManagementSystem
             txtPassword = new TextBox { PlaceholderText = "Password", UseSystemPasswordChar = true };
             btnLogin = new Button { Text = "Login" };
             lblStatus = new Label { AutoSize = true };
-            btnLogin.Click += BtnLogin_Click;
-
         }
 
         private void txtUsername_TextChanged(object sender, EventArgs e)

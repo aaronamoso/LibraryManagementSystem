cce721a15ead814cc61721b10c529f335f763e86
JNevile <j.neville657@mybvc.ca>
2024-12-09 20:13:13 -0700

    updates to forms to login as admin or user

---
+++ DashboardUser.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace LibraryManagementSystem
+{
+    partial class DashboardUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(250, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(274, 48);
+            label1.TabIndex = 0;
+            label1.Text = "User Dashboard";
+            // 
+            // DashboardUser
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "DashboardUser";
+            Text = "DashboardUser";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
---
+++ DashboardUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LibraryManagementSystem
+{
+    public partial class DashboardUser : Form
+    {
+        public DashboardUser()
+        {
+            InitializeComponent();
+        }
+
+        //Add more custom methods or properties here for the user dashboard
+    }
+}

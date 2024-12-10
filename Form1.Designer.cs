f2914bd5c779d0bbe2cc3f8e2d9321f9a1151138
JNevile <j.neville657@mybvc.ca>
2024-12-09 22:20:31 -0700

    add colors to login form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,17 +38,20 @@
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(171, 255);
+            btnLogin.BackColor = Color.FromArgb(139, 50, 44);
+            btnLogin.Font = new Font("Franklin Gothic Medium Cond", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLogin.ForeColor = Color.FromArgb(234, 206, 167);
+            btnLogin.Location = new Point(170, 226);
             btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(111, 33);
+            btnLogin.Size = new Size(123, 56);
             btnLogin.TabIndex = 0;
             btnLogin.Text = "Login";
-            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.UseVisualStyleBackColor = false;
             btnLogin.Click += btnLogin_Click;
             // 
             // txtUsername
             // 
-            txtUsername.Location = new Point(241, 97);
+            txtUsername.Location = new Point(241, 100);
             txtUsername.Name = "txtUsername";
             txtUsername.Size = new Size(150, 31);
             txtUsername.TabIndex = 1;
@@ -56,7 +59,7 @@
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(241, 158);
+            txtPassword.Location = new Point(241, 161);
             txtPassword.Name = "txtPassword";
             txtPassword.Size = new Size(150, 31);
             txtPassword.TabIndex = 2;
@@ -64,34 +67,41 @@
             // lblUsername
             // 
             lblUsername.AutoSize = true;
+            lblUsername.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblUsername.ForeColor = Color.FromArgb(139, 50, 44);
             lblUsername.Location = new Point(84, 97);
             lblUsername.Name = "lblUsername";
-            lblUsername.Size = new Size(95, 25);
+            lblUsername.Size = new Size(126, 32);
             lblUsername.TabIndex = 3;
             lblUsername.Text = "Username:";
             // 
             // lblPassword
             // 
             lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPassword.ForeColor = Color.FromArgb(139, 50, 44);
             lblPassword.Location = new Point(84, 158);
             lblPassword.Name = "lblPassword";
-            lblPassword.Size = new Size(91, 25);
+            lblPassword.Size = new Size(116, 32);
             lblPassword.TabIndex = 4;
             lblPassword.Text = "Password:";
             // 
             // lblStatus
             // 
             lblStatus.AutoSize = true;
-            lblStatus.Location = new Point(93, 37);
+            lblStatus.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblStatus.ForeColor = Color.FromArgb(139, 50, 44);
+            lblStatus.Location = new Point(170, 28);
             lblStatus.Name = "lblStatus";
-            lblStatus.Size = new Size(59, 25);
+            lblStatus.Size = new Size(108, 48);
             lblStatus.TabIndex = 5;
-            lblStatus.Text = "label1";
+            lblStatus.Text = "Login";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(234, 206, 167);
             ClientSize = new Size(479, 337);
             Controls.Add(lblStatus);
             Controls.Add(lblPassword);

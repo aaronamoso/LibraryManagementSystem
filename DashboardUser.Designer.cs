a9c9a24833e6776dc299811fa3553faabef3e860
JNevile <j.neville657@mybvc.ca>
2024-12-10 20:50:17 -0700

    add library catalogue book listing to user dashboard that appears after login.  intention is that when a user double clicks on a book name, it will generate the form BookDetails.cs and that form will appear with ll the book details

--- DashboardUser.Designer.cs
+++ DashboardUser.Designer.cs
@@ -29,17 +29,40 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            lbLibraryBooks = new ListBox();
+            label2 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(250, 84);
+            label1.Location = new Point(253, 26);
             label1.Name = "label1";
             label1.Size = new Size(274, 48);
             label1.TabIndex = 0;
             label1.Text = "User Dashboard";
+            // 
+            // lbLibraryBooks
+            // 
+            lbLibraryBooks.FormattingEnabled = true;
+            lbLibraryBooks.HorizontalScrollbar = true;
+            lbLibraryBooks.ItemHeight = 25;
+            lbLibraryBooks.Items.AddRange(new object[] { "Destined for Murder", "Bringers of the Dawn", "You Cannot Die", "Many Mansions" });
+            lbLibraryBooks.Location = new Point(36, 167);
+            lbLibraryBooks.Name = "lbLibraryBooks";
+            lbLibraryBooks.Size = new Size(314, 129);
+            lbLibraryBooks.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(36, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(201, 32);
+            label2.TabIndex = 2;
+            label2.Text = "Library catalogue:";
             // 
             // DashboardUser
             // 
@@ -47,6 +70,8 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(234, 206, 167);
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(lbLibraryBooks);
             Controls.Add(label1);
             ForeColor = Color.FromArgb(139, 50, 44);
             Name = "DashboardUser";
@@ -58,5 +83,7 @@
         #endregion
 
         private Label label1;
+        private ListBox lbLibraryBooks;
+        private Label label2;
     }
 }

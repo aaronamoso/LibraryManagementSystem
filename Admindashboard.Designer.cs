b6a363874dc25f390825fad4e346cc3e8f11d999
Aaron Amoso <amosoaaron@gmail.com>
2024-12-10 23:25:15 -0700

    Revert "finished WF for Admin Dash and deleted AdminDashboard to prevent confusion"
    This reverts commit eda6fafb1966a30bd72b7e2d9e4ee65071d9dc59.

---
+++ Admindashboard.Designer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace LibraryManagementSystem
+{
+    public partial class Admindashboard : Form
+    {
+        private List<LibraryItem> libraryItems = new List<LibraryItem>();
+        private List<UserAccount> userAccounts = new List<UserAccount>();
+
+        private Button btnAddItem;
+        private Button btnEditUser;
+        private Button btnViewOverdue;
+        private DataGridView dgvLibraryItems;
+        private Label label1;
+        private TextBox txtOutput;
+
+        public Admindashboard()
+        {
+            InitializeComponent();
+            LoadTestData();
+        }
+        private void LoadTestData()
+        {
+            // Add test library items
+            libraryItems.Add(new LibraryItem { Title = "Book 1", IsOverdue = false });
+            libraryItems.Add(new LibraryItem { Title = "Book 2", IsOverdue = true });
+
+            // Add test user accounts
+            userAccounts.Add(new UserAccount { Name = "John Doe" });
+
+            UpdateDataGrid();
+        }
+
+        private void UpdateDataGrid()
+        {
+            dgvLibraryItems.DataSource = null;
+            dgvLibraryItems.DataSource = libraryItems.Select(x => new { x.Title, x.IsOverdue }).ToList();
+        }
+
+        private void btnAddItem_Click(object sender, EventArgs e)
+        {
+            libraryItems.Add(new LibraryItem { Title = "New Book", IsOverdue = false });
+            txtOutput.Text = "New item added.";
+            UpdateDataGrid();
+        }
+
+        private void btnEditUser_Click(object sender, EventArgs e)
+        {
+            userAccounts[0].Name = "Jane Doe";
+            txtOutput.Text = "User account edited.";
+        }
+
+        private void btnViewOverdue_Click(object sender, EventArgs e)
+        {
+            var overdueItems = libraryItems.Where(x => x.IsOverdue).Select(x => x.Title).ToList();
+            txtOutput.Text = "Overdue Items: " + string.Join(", ", overdueItems);
+        }
+    }
+
+    public class LibraryItem
+    {
+        public string Title { get; set; }
+        public bool IsOverdue { get; set; }
+    }
+
+    public class UserAccount
+    {
+        public string Name { get; set; }
+    }
+
+
+    //#region Windows Form Designer generated code
+
+    ///// <summary>
+    ///// Required method for Designer support - do not modify
+    ///// the contents of this method with the code editor.
+    ///// </summary>
+    //private void InitializeComponent()
+    //{
+    //    this.components = new System.ComponentModel.Container();
+    //    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+    //    this.ClientSize = new System.Drawing.Size(800, 450);
+    //    this.Text = "Admindashboard";
+    //}
+
+    //#endregion
+}
---
+++ Admindashboard.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagementSystem
+{
+    public partial class Admindashboard : Form
+    {
+ 
+        private void InitializeComponent()
+        {
+            btnAddItem = new Button();
+            btnEditUser = new Button();
+            btnViewOverdue = new Button();
+            dgvLibraryItems = new DataGridView();
+            label1 = new Label();
+            txtOutput = new TextBox();
+            ((ISupportInitialize)dgvLibraryItems).BeginInit();
+            SuspendLayout();
+            // 
+            // btnAddItem
+            // 
+            btnAddItem.Location = new Point(30, 30);
+            btnAddItem.Name = "btnAddItem";
+            btnAddItem.Size = new Size(150, 50);
+            btnAddItem.TabIndex = 0;
+            btnAddItem.Text = "Add/Edit Items";
+            btnAddItem.UseVisualStyleBackColor = true;
+            btnAddItem.Click += btnAddItem_Click;
+            // 
+            // btnEditUser
+            // 
+            btnEditUser.Location = new Point(30, 100);
+            btnEditUser.Name = "btnEditUser";
+            btnEditUser.Size = new Size(150, 50);
+            btnEditUser.TabIndex = 1;
+            btnEditUser.Text = "Add/Edit Users";
+            btnEditUser.UseVisualStyleBackColor = true;
+            btnEditUser.Click += btnEditUser_Click;
+            // 
+            // btnViewOverdue
+            // 
+            btnViewOverdue.Location = new Point(30, 170);
+            btnViewOverdue.Name = "btnViewOverdue";
+            btnViewOverdue.Size = new Size(150, 50);
+            btnViewOverdue.TabIndex = 2;
+            btnViewOverdue.Text = "View Overdue Items";
+            btnViewOverdue.UseVisualStyleBackColor = true;
+            btnViewOverdue.Click += btnViewOverdue_Click;
+            // 
+            // dgvLibraryItems
+            // 
+            dgvLibraryItems.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvLibraryItems.Location = new Point(200, 30);
+            dgvLibraryItems.Name = "dgvLibraryItems";
+            dgvLibraryItems.Size = new Size(500, 300);
+            dgvLibraryItems.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(105, 293);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 4;
+            // 
+            // txtOutput
+            // 
+            txtOutput.Location = new Point(200, 350);
+            txtOutput.Multiline = true;
+            txtOutput.Name = "txtOutput";
+            txtOutput.Size = new Size(500, 50);
+            txtOutput.TabIndex = 5;
+            // 
+            // Admindashboard
+            // 
+            ClientSize = new Size(871, 547);
+            Controls.Add(txtOutput);
+            Controls.Add(label1);
+            Controls.Add(dgvLibraryItems);
+            Controls.Add(btnViewOverdue);
+            Controls.Add(btnEditUser);
+            Controls.Add(btnAddItem);
+            Name = "Admindashboard";
+            Text = "View Overdue Items";
+            ((ISupportInitialize)dgvLibraryItems).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}

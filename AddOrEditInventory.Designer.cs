f42280ccb9ce30508800f3ba14067bb096d910d3
Aaron Amoso <amosoaaron@gmail.com>
2024-12-12 21:56:02 -0700

    Add and Edit popup UI

---
+++ AddOrEditInventory.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace LibraryManagementSystem
+{
+    partial class AddOrEditInventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBoxAuthor = new TextBox();
+            textBoxDatePublished = new TextBox();
+            textBoxGenre = new TextBox();
+            textBoxISBN = new TextBox();
+            textBoxDescription = new TextBox();
+            buttonAdd = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(139, 50, 44);
+            label1.Location = new Point(184, 124);
+            label1.Name = "label1";
+            label1.Size = new Size(246, 44);
+            label1.TabIndex = 0;
+            label1.Text = "Add Inventory";
+            // 
+            // textBoxAuthor
+            // 
+            textBoxAuthor.ForeColor = Color.FromArgb(139, 50, 44);
+            textBoxAuthor.Location = new Point(121, 171);
+            textBoxAuthor.Name = "textBoxAuthor";
+            textBoxAuthor.Size = new Size(279, 23);
+            textBoxAuthor.TabIndex = 1;
+            textBoxAuthor.Text = "Author";
+            // 
+            // textBoxDatePublished
+            // 
+            textBoxDatePublished.ForeColor = Color.FromArgb(139, 50, 44);
+            textBoxDatePublished.Location = new Point(121, 216);
+            textBoxDatePublished.Name = "textBoxDatePublished";
+            textBoxDatePublished.Size = new Size(279, 23);
+            textBoxDatePublished.TabIndex = 2;
+            textBoxDatePublished.Text = "Date Published";
+            // 
+            // textBoxGenre
+            // 
+            textBoxGenre.ForeColor = Color.FromArgb(139, 50, 44);
+            textBoxGenre.Location = new Point(121, 262);
+            textBoxGenre.Name = "textBoxGenre";
+            textBoxGenre.Size = new Size(279, 23);
+            textBoxGenre.TabIndex = 3;
+            textBoxGenre.Text = "Genre";
+            // 
+            // textBoxISBN
+            // 
+            textBoxISBN.ForeColor = Color.FromArgb(139, 50, 44);
+            textBoxISBN.Location = new Point(121, 306);
+            textBoxISBN.Name = "textBoxISBN";
+            textBoxISBN.Size = new Size(279, 23);
+            textBoxISBN.TabIndex = 4;
+            textBoxISBN.Text = "ISBN #";
+            // 
+            // textBoxDescription
+            // 
+            textBoxDescription.ForeColor = Color.FromArgb(139, 50, 44);
+            textBoxDescription.Location = new Point(121, 351);
+            textBoxDescription.Name = "textBoxDescription";
+            textBoxDescription.Size = new Size(279, 23);
+            textBoxDescription.TabIndex = 5;
+            textBoxDescription.Text = "Description";
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.BackColor = Color.FromArgb(139, 50, 44);
+            buttonAdd.ForeColor = Color.FromArgb(234, 206, 167);
+            buttonAdd.Location = new Point(211, 406);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(85, 37);
+            buttonAdd.TabIndex = 6;
+            buttonAdd.Text = "Add";
+            buttonAdd.UseVisualStyleBackColor = false;
+            // 
+            // AddOrEditInventory
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(234, 206, 167);
+            ClientSize = new Size(540, 546);
+            Controls.Add(buttonAdd);
+            Controls.Add(textBoxDescription);
+            Controls.Add(textBoxISBN);
+            Controls.Add(textBoxGenre);
+            Controls.Add(textBoxDatePublished);
+            Controls.Add(textBoxAuthor);
+            Controls.Add(label1);
+            Name = "AddOrEditInventory";
+            Text = "AddOrEditInventory";
+            Load += AddOrEditInventory_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBoxAuthor;
+        private TextBox textBoxDatePublished;
+        private TextBox textBoxGenre;
+        private TextBox textBoxISBN;
+        private TextBox textBoxDescription;
+        private Button buttonAdd;
+    }
+}
---
+++ EditInventory.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace LibraryManagementSystem
+{
+    partial class EditInventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonEdit = new Button();
+            textBoxDescriptionEdit = new TextBox();
+            textBoxISBNEdit = new TextBox();
+            textBoxGenreEdit = new TextBox();
+            textBoxDatePublishedEdit = new TextBox();
+            textBoxAuthorEdit = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // buttonEdit
+            // 
+            buttonEdit.BackColor = Color.FromArgb(139, 50, 44);
+            buttonEdit.ForeColor = Color.FromArgb(234, 206, 167);
+            buttonEdit.Location = new Point(261, 339);
+            buttonEdit.Name = "buttonEdit";
+            buttonEdit.Size = new Size(123, 37);
+            buttonEdit.TabIndex = 13;
+            buttonEdit.Text = "Submit Changes";
+            buttonEdit.UseVisualStyleBackColor = false;
+            // 
+            // textBoxDescriptionEdit
+            // 
+            textBoxDescriptionEdit.ForeColor = Color.FromArgb(139, 50, 44);
+            textBoxDescriptionEdit.Location = new Point(188, 296);
+            textBoxDescriptionEdit.Name = "textBoxDescriptionEdit";
+            textBoxDescriptionEdit.Size = new Size(279, 23);
+            textBoxDescriptionEdit.TabIndex = 12;
+            textBoxDescriptionEdit.Text = "Description";
+            // 
+            // textBoxISBNEdit
+            // 
+            textBoxISBNEdit.ForeColor = Color.FromArgb(139, 50, 44);
+            textBoxISBNEdit.Location = new Point(188, 248);
+            textBoxISBNEdit.Name = "textBoxISBNEdit";
+            textBoxISBNEdit.Size = new Size(279, 23);
+            textBoxISBNEdit.TabIndex = 11;
+            textBoxISBNEdit.Text = "ISBN #";
+            // 
+            // textBoxGenreEdit
+            // 
+            textBoxGenreEdit.ForeColor = Color.FromArgb(139, 50, 44);
+            textBoxGenreEdit.Location = new Point(188, 204);
+            textBoxGenreEdit.Name = "textBoxGenreEdit";
+            textBoxGenreEdit.Size = new Size(279, 23);
+            textBoxGenreEdit.TabIndex = 10;
+            textBoxGenreEdit.Text = "Genre";
+            // 
+            // textBoxDatePublishedEdit
+            // 
+            textBoxDatePublishedEdit.ForeColor = Color.FromArgb(139, 50, 44);
+            textBoxDatePublishedEdit.Location = new Point(188, 158);
+            textBoxDatePublishedEdit.Name = "textBoxDatePublishedEdit";
+            textBoxDatePublishedEdit.Size = new Size(279, 23);
+            textBoxDatePublishedEdit.TabIndex = 9;
+            textBoxDatePublishedEdit.Text = "Date Published";
+            // 
+            // textBoxAuthorEdit
+            // 
+            textBoxAuthorEdit.ForeColor = Color.FromArgb(139, 50, 44);
+            textBoxAuthorEdit.Location = new Point(188, 113);
+            textBoxAuthorEdit.Name = "textBoxAuthorEdit";
+            textBoxAuthorEdit.Size = new Size(279, 23);
+            textBoxAuthorEdit.TabIndex = 8;
+            textBoxAuthorEdit.Text = "Author";
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(139, 50, 44);
+            label1.Location = new Point(248, 66);
+            label1.Name = "label1";
+            label1.Size = new Size(156, 44);
+            label1.TabIndex = 7;
+            label1.Text = "Edit Inventory";
+            // 
+            // EditInventory
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(234, 206, 167);
+            ClientSize = new Size(685, 450);
+            Controls.Add(buttonEdit);
+            Controls.Add(textBoxDescriptionEdit);
+            Controls.Add(textBoxISBNEdit);
+            Controls.Add(textBoxGenreEdit);
+            Controls.Add(textBoxDatePublishedEdit);
+            Controls.Add(textBoxAuthorEdit);
+            Controls.Add(label1);
+            Name = "EditInventory";
+            Text = "EditInventory";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonEdit;
+        private TextBox textBoxDescriptionEdit;
+        private TextBox textBoxISBNEdit;
+        private TextBox textBoxGenreEdit;
+        private TextBox textBoxDatePublishedEdit;
+        private TextBox textBoxAuthorEdit;
+        private Label label1;
+    }
+}

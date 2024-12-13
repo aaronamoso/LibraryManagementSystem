51e27166638d9a04d88cbb445eddeed874dffd43
Aaron Amoso <amosoaaron@gmail.com>
2024-12-13 00:49:23 -0700

    Touched a lot of files. Overall working but not mainly focusing on editing the current book.

--- DashboardAdmin.cs
+++ DashboardAdmin.cs
@@ -1,14 +1,71 @@
 ﻿using System;
 using System.Windows.Forms;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace LibraryManagementSystem
 {
     public partial class DashboardAdmin : Form
     {
+        private List<Book> books;
+        private List<Book> borrowedBooks;
+        private BindingSource catalogueBinding;
+        private BindingSource borrowedBooksBinding;
         public DashboardAdmin()
         {
             // Call to method is OK, just not definition
             InitializeComponent();
+            books = LibraryCatalogue.GetBooks();
+
+            catalogueBinding = new BindingSource();
+            catalogueBinding.DataSource = books;
+
+            lbLibraryCatalogue.DataSource = catalogueBinding;
+            lbLibraryCatalogue.DisplayMember = "Title";
+
+            // SEARCH BAR
+            lbLibraryCatalogue.MouseDoubleClick += lbLibraryCatalogue_MouseDoubleClick;
+            // LOGOUT
+            btnLogout.Click += btnLogout_Click;
+        }
+
+        private void btnAddOrEditInventory_Click(object sender, EventArgs e)
+        {
+            //Navigate the edit button from DashboardAdmin to AddOrEditInventory.
+            AddOrEditInventory addOrEditInventory = new AddOrEditInventory(); // creates an instance
+
+            addOrEditInventory.Show(); //shows the form
+        }
+
+       
+        private void lbLibraryCatalogue_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (lbLibraryCatalogue.SelectedItem is Book selectedBook)
+            {
+                var bookDetails = new BookDetails(selectedBook);
+                bookDetails.Show();
+            }
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            //When user clicks the Logout button, first shows them a message asking for confirmation:
+            DialogResult result = MessageBox.Show("Are you sure you want to logout?",
+                                                "Logout Confirmation",
+                                                MessageBoxButtons.YesNo,
+                                                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                //If the user says yes to the confirmation message, this hides the current form
+                this.Hide();
+
+                //And brings the user back to the homepage login screen
+                Form Login = new Login();
+                Login.Show();
+
+                //Lastly, closes the user dashboard upon logout
+                this.Close();
+            }
         }
     }
 }
--- EditInventory.Designer.cs
+++ EditInventory.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace LibraryManagementSystem
+﻿
+namespace LibraryManagementSystem
 {
     partial class EditInventory
     {
@@ -28,108 +29,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            buttonEdit = new Button();
-            textBoxDescriptionEdit = new TextBox();
-            textBoxISBNEdit = new TextBox();
-            textBoxGenreEdit = new TextBox();
-            textBoxDatePublishedEdit = new TextBox();
-            textBoxAuthorEdit = new TextBox();
             label1 = new Label();
+            btnChangeCover = new Button();
+            txtAuthor = new TextBox();
+            txtBookName = new TextBox();
+            txtPublished = new TextBox();
+            txtISBN = new TextBox();
+            txtDescription = new TextBox();
+            txtGenre = new TextBox();
+            pbBookCover = new PictureBox();
+            btnSaveChanges = new Button();
+            btnCancel = new Button();
+            ((System.ComponentModel.ISupportInitialize)pbBookCover).BeginInit();
             SuspendLayout();
-            // 
-            // buttonEdit
-            // 
-            buttonEdit.BackColor = Color.FromArgb(139, 50, 44);
-            buttonEdit.ForeColor = Color.FromArgb(234, 206, 167);
-            buttonEdit.Location = new Point(261, 339);
-            buttonEdit.Name = "buttonEdit";
-            buttonEdit.Size = new Size(123, 37);
-            buttonEdit.TabIndex = 13;
-            buttonEdit.Text = "Submit Changes";
-            buttonEdit.UseVisualStyleBackColor = false;
-            // 
-            // textBoxDescriptionEdit
-            // 
-            textBoxDescriptionEdit.ForeColor = Color.FromArgb(139, 50, 44);
-            textBoxDescriptionEdit.Location = new Point(188, 296);
-            textBoxDescriptionEdit.Name = "textBoxDescriptionEdit";
-            textBoxDescriptionEdit.Size = new Size(279, 23);
-            textBoxDescriptionEdit.TabIndex = 12;
-            textBoxDescriptionEdit.Text = "Description";
-            // 
-            // textBoxISBNEdit
-            // 
-            textBoxISBNEdit.ForeColor = Color.FromArgb(139, 50, 44);
-            textBoxISBNEdit.Location = new Point(188, 248);
-            textBoxISBNEdit.Name = "textBoxISBNEdit";
-            textBoxISBNEdit.Size = new Size(279, 23);
-            textBoxISBNEdit.TabIndex = 11;
-            textBoxISBNEdit.Text = "ISBN #";
-            // 
-            // textBoxGenreEdit
-            // 
-            textBoxGenreEdit.ForeColor = Color.FromArgb(139, 50, 44);
-            textBoxGenreEdit.Location = new Point(188, 204);
-            textBoxGenreEdit.Name = "textBoxGenreEdit";
-            textBoxGenreEdit.Size = new Size(279, 23);
-            textBoxGenreEdit.TabIndex = 10;
-            textBoxGenreEdit.Text = "Genre";
-            // 
-            // textBoxDatePublishedEdit
-            // 
-            textBoxDatePublishedEdit.ForeColor = Color.FromArgb(139, 50, 44);
-            textBoxDatePublishedEdit.Location = new Point(188, 158);
-            textBoxDatePublishedEdit.Name = "textBoxDatePublishedEdit";
-            textBoxDatePublishedEdit.Size = new Size(279, 23);
-            textBoxDatePublishedEdit.TabIndex = 9;
-            textBoxDatePublishedEdit.Text = "Date Published";
-            // 
-            // textBoxAuthorEdit
-            // 
-            textBoxAuthorEdit.ForeColor = Color.FromArgb(139, 50, 44);
-            textBoxAuthorEdit.Location = new Point(188, 113);
-            textBoxAuthorEdit.Name = "textBoxAuthorEdit";
-            textBoxAuthorEdit.Size = new Size(279, 23);
-            textBoxAuthorEdit.TabIndex = 8;
-            textBoxAuthorEdit.Text = "Author";
             // 
             // label1
             // 
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.FromArgb(139, 50, 44);
-            label1.Location = new Point(248, 66);
+            label1.Location = new Point(149, 48);
             label1.Name = "label1";
-            label1.Size = new Size(156, 44);
+            label1.Size = new Size(156, 31);
             label1.TabIndex = 7;
             label1.Text = "Edit Inventory";
+            // 
+            // btnChangeCover
+            // 
+            btnChangeCover.BackColor = Color.FromArgb(139, 50, 44);
+            btnChangeCover.ForeColor = Color.FromArgb(234, 206, 167);
+            btnChangeCover.Location = new Point(80, 215);
+            btnChangeCover.Name = "btnChangeCover";
+            btnChangeCover.Size = new Size(142, 30);
+            btnChangeCover.TabIndex = 21;
+            btnChangeCover.Text = "Change Cover";
+            btnChangeCover.UseVisualStyleBackColor = false;
+            //btnChangeCover.Click += buttonSaveEdit_Click;
+            // 
+            // txtAuthor
+            // 
+            txtAuthor.Location = new Point(228, 135);
+            txtAuthor.Name = "txtAuthor";
+            txtAuthor.Size = new Size(142, 23);
+            txtAuthor.TabIndex = 22;
+            // 
+            // txtBookName
+            // 
+            txtBookName.Location = new Point(228, 106);
+            txtBookName.Name = "txtBookName";
+            txtBookName.Size = new Size(142, 23);
+            txtBookName.TabIndex = 23;
+            // 
+            // txtPublished
+            // 
+            txtPublished.Location = new Point(228, 193);
+            txtPublished.Name = "txtPublished";
+            txtPublished.Size = new Size(142, 23);
+            txtPublished.TabIndex = 25;
+            // 
+            // txtISBN
+            // 
+            txtISBN.Location = new Point(228, 164);
+            txtISBN.Name = "txtISBN";
+            txtISBN.Size = new Size(142, 23);
+            txtISBN.TabIndex = 24;
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new Point(80, 251);
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(290, 23);
+            txtDescription.TabIndex = 27;
+            // 
+            // txtGenre
+            // 
+            txtGenre.Location = new Point(228, 222);
+            txtGenre.Name = "txtGenre";
+            txtGenre.Size = new Size(142, 23);
+            txtGenre.TabIndex = 26;
+            // 
+            // pbBookCover
+            // 
+            pbBookCover.Location = new Point(80, 108);
+            pbBookCover.Name = "pbBookCover";
+            pbBookCover.Size = new Size(142, 101);
+            pbBookCover.TabIndex = 28;
+            pbBookCover.TabStop = false;
+            // 
+            // btnSaveChanges
+            // 
+            btnSaveChanges.BackColor = Color.FromArgb(139, 50, 44);
+            btnSaveChanges.ForeColor = Color.FromArgb(234, 206, 167);
+            btnSaveChanges.Location = new Point(80, 282);
+            btnSaveChanges.Name = "btnSaveChanges";
+            btnSaveChanges.Size = new Size(142, 30);
+            btnSaveChanges.TabIndex = 29;
+            btnSaveChanges.Text = "Save Changes";
+            btnSaveChanges.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            btnCancel.BackColor = Color.FromArgb(139, 50, 44);
+            btnCancel.ForeColor = Color.FromArgb(234, 206, 167);
+            btnCancel.Location = new Point(228, 282);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(142, 30);
+            btnCancel.TabIndex = 30;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = false;
             // 
             // EditInventory
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(234, 206, 167);
-            ClientSize = new Size(685, 450);
-            Controls.Add(buttonEdit);
-            Controls.Add(textBoxDescriptionEdit);
-            Controls.Add(textBoxISBNEdit);
-            Controls.Add(textBoxGenreEdit);
-            Controls.Add(textBoxDatePublishedEdit);
-            Controls.Add(textBoxAuthorEdit);
+            ClientSize = new Size(492, 428);
+            Controls.Add(btnCancel);
+            Controls.Add(btnSaveChanges);
+            Controls.Add(pbBookCover);
+            Controls.Add(txtDescription);
+            Controls.Add(txtGenre);
+            Controls.Add(txtPublished);
+            Controls.Add(txtISBN);
+            Controls.Add(txtBookName);
+            Controls.Add(txtAuthor);
+            Controls.Add(btnChangeCover);
             Controls.Add(label1);
             Name = "EditInventory";
             Text = "EditInventory";
+            ((System.ComponentModel.ISupportInitialize)pbBookCover).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
-        #endregion
+        private void button_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
 
-        private Button buttonEdit;
+#endregion
+        private Label label1;
+        private PictureBox bookCoverEdit;
+        private TextBox textBoxBookNameEdit;
         private TextBox textBoxDescriptionEdit;
         private TextBox textBoxISBNEdit;
         private TextBox textBoxGenreEdit;
         private TextBox textBoxDatePublishedEdit;
         private TextBox textBoxAuthorEdit;
-        private Label label1;
+        private Button btnChangeCover;
+        private TextBox txtAuthor;
+        private TextBox txtBookName;
+        private TextBox txtPublished;
+        private TextBox txtISBN;
+        private TextBox txtDescription;
+        private TextBox txtGenre;
+        private PictureBox pbBookCover;
+        private Button btnSaveChanges;
+        private Button btnCancel;
     }
 }

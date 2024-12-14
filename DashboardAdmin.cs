beea1070404c518dbc58d688dc6d2ba840080a60
JNevile <j.neville657@mybvc.ca>
2024-12-13 18:56:46 -0700

    add logout ability to Admin dashboard

--- DashboardAdmin.cs
+++ DashboardAdmin.cs
@@ -9,6 +9,31 @@ namespace LibraryManagementSystem
         {
             // Call to method is OK, just not definition
             InitializeComponent();
+            btnLogout.Click += btnLogout_Click;
+        }
+
+
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            //When user clicks the Logout button, first shows them a message asking for confirmation:
+            DialogResult result = MessageBox.Show("Are you sure you want to logout?",
+                                                "Logout Confirmation",
+                                                MessageBoxButtons.YesNo,
+                                                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                //If the user says yes to the confirmation message, this hides the current form
+                this.Hide();
+
+                //And brings the user back to the homepage login screen
+                Form Login = new Login();
+                Login.Show();
+
+                //Lastly, closes the user dashboard upon logout
+                this.Close();
+            }
         }
     }
 }
